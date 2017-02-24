c6a0c5f01750d1e9ff73ad921aef95b87f5beaa6
gapariciogro <gilbertoag@cequality.com>
2017-02-24 08:22:40 -0600

    Ejemplo de literales enteras

---
+++ src/com/cequality/IntegerLiterals.java
@@ -0,0 +1,20 @@
+package com.cequality;
+
+/**
+ *
+ * @author Gilberto
+ */
+public class IntegerLiterals {
+    public static void main(String... args) {
+        int decimal = 10;
+        int hexadecimal = 0xA;
+        int octal = 012;
+        int binario = 0b1010;
+        
+        System.out.println(decimal);
+        System.out.println(hexadecimal);
+        System.out.println(octal);
+        System.out.println(binario);
+        
+    }
+}
